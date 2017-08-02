01d957f03cfdcdce0462fc383d72436bbcf539e7
Noah Reardon <noahreardon@gmail.com>
2017-08-02 17:25:02 -0500

    basic movement

---
+++ Iron-Oasis-Unity/Iron Oasis/Assets/Scripts/CameraScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour {
+
+    public Transform target;
+
+    public Vector3 offset;
+
+    public float pitch = 2f;
+
+    private float currentZoom = 10f;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        transform.position = target.position - offset * currentZoom;
+        transform.LookAt(target.position + Vector3.up * pitch);
+	}
+}
---
+++ Iron-Oasis-Unity/Iron Oasis/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour {
+
+    public float speed = 1;
+
+    private bool isMoving;
+    private Rigidbody rb;
+    private float inputValueHorizontal;
+    private float inputValueVertical;
+
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void OnEnable()
+    {
+        rb.isKinematic = false;
+    }
+
+    void OnDisable()
+    {
+        rb.isKinematic = true;
+    }
+
+    // Use this for initialization
+    void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Mathf.Abs(Input.GetAxisRaw("Horizontal")) > 0.1 || Mathf.Abs(Input.GetAxisRaw("Vertical")) > 0.1)
+        {
+            isMoving = true;
+            inputValueHorizontal = Input.GetAxisRaw("Horizontal");
+            inputValueVertical = Input.GetAxisRaw("Vertical");
+        }
+        else
+        {
+            isMoving = false;
+        }
+
+        Debug.Log(Input.GetAxisRaw("Horizontal") + "    " + Input.GetAxisRaw("Vertical"));
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+        Turn();
+    }
+
+    private void Move()
+    {
+        Vector3 move;
+        if (isMoving)
+        {
+            move = transform.forward * speed * Time.deltaTime;
+        }
+        else
+        {
+            move = Vector3.zero;
+        }
+            
+        rb.MovePosition(rb.position + move);
+    }
+
+    private void Turn()
+    {
+        Vector3 targetAngle = new Vector3(inputValueHorizontal, 0f, inputValueVertical);
+
+        Quaternion turn = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(targetAngle), Time.deltaTime * 10f);
+
+        transform.rotation = turn;
+    }
+}
